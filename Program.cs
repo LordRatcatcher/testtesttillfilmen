48d10619c7ea69e8172a0459aa292dbad96a9f47
ÄGARE <GARE@DESKTOP-1SVJT58>
2020-10-07 09:44:44 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace testtesttillfilmen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("once upon a time");
+            string grönman = Console.ReadLine();
+            Console.WriteLine(grönman);
+            
+            
+        }
+    }
+}
